d69f0b4d2ef9d911aaf0345f8e52679cb38e3044
Mahir4098 <mahirjr98@gmail.com>
2026-04-26 13:43:35 +0600

    Implement Checkout Server Api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 
 // middleware
 
@@ -42,7 +44,7 @@ async function run() {
 
         })
 
-        app.get('parcels/:id', async (req, res) => {
+        app.get('/parcels/:id', async (req, res) => {
             const id = req.params.id;
 
             const query = { _id: new ObjectId(id) }
@@ -64,6 +66,44 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await parcelCollection.deleteOne(query)
             res.send(result);
+        })
+
+        // Payment Related Api
+
+        app.post('/create-checkout-session', async (req, res) => {
+            const paymentInfo = req.body
+
+            const amount = parseInt(paymentInfo.cost) * 100;
+
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+                        price_data: {
+                            currency: 'USD',
+                            unit_amount: amount,
+                            product_data: {
+                                name: paymentInfo.parcelName
+                            }
+
+                        },
+
+                        quantity: 1,
+                    },
+                ],
+                customer_email: paymentInfo.senderEmail,
+
+                mode: 'payment',
+                metadata: {
+                    parcelId: paymentInfo.parcelId
+                },
+                success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+
+            });
+
+            console.log(session)
+            res.send({ url: session.url })
         })
 
         // Connect the client to the server	(optional starting in v4.7)
